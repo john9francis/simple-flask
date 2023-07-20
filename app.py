d126c66547a1faa1846eae1a684f24597eb996a6
John Francis <john9francis@gmail.com>
2023-07-19 23:24:39 -0600

    added a new url /test/ just to learn how to map onto different urls

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
     return "Hello World, from Flask!"
+
+@app.route("/test/")
+def test():
+    return "test test test"
+
+
+if __name__ == "__main__":
+    app.run()
